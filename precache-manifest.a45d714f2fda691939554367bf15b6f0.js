c6c464cbdbd9452d960360d83574598f45396a64
Neha Gupta <er.nehaa.gupta@gmail.com>
2020-02-23 21:07:54 -0500

    Updates

---
+++ precache-manifest.a45d714f2fda691939554367bf15b6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1848d8931fce11040eed5a29b82a733",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95f8fd4d04c3576137e3",
+    "url": "/monsters-rolodex/static/css/main.73e0c7d4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f8fd4d04c3576137e3",
+    "url": "/monsters-rolodex/static/js/main.a834678c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
